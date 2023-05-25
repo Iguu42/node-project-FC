9207560178752ac411535cf0887f88fa04c356f9
Igor Félix <igor.pereira@avenuecode.com>
2023-05-25 20:54:53 -0300

    .

--- src/routes/pedidos.routes.ts
+++ src/routes/pedidos.routes.ts
@@ -1,9 +1,9 @@
 import { GetPedidoPorCpfController } from '../modules/pedidos/buscarPedidosPorCPF/GetPedidoPorCpfController';
-//import { GetAllPedidosController } from '../modules/pedidos/buscarTodosPedidos/GetAllPedidosController';
+import { GetAllPedidosController } from '../modules/pedidos/buscarTodosPedidos/GetAllPedidosController';
 import { Router } from "express";
 
 const pedidosRoutes = Router();
 
-//pedidosRoutes.get("/all", new GetAllPedidosController().handle);
+pedidosRoutes.get("/alll", new GetAllPedidosController().handle);
 pedidosRoutes.get("/:cpf", new GetPedidoPorCpfController().handle);
 export { pedidosRoutes };

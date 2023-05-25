db5884c841ff4b9e23676ad7bcdae13e8182ad97
Igor Félix <igor.pereira@avenuecode.com>
2023-05-25 20:35:18 -0300

    .

--- src/routes/pedidos.routes.ts
+++ src/routes/pedidos.routes.ts
@@ -1,5 +1,4 @@
 import { GetPedidoPorCpfController } from '../modules/pedidos/buscarPedidosPorCPF/GetPedidoPorCpfController';
-import { GetPedidoPorNumeroController } from '../modules/pedidos/buscarPedidoPorNumero/GetPedidoPorNumeroController';
 import { GetAllPedidosController } from '../modules/pedidos/buscarTodosPedidos/GetAllPedidosController';
 import { Router } from "express";
 

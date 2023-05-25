d766dc7e1ab3093b0f53d5ccc50f5a68c665476c
Igor Félix <igor.pereira@avenuecode.com>
2023-05-25 20:39:36 -0300

    .

--- src/routes/empresa.routes.ts
+++
@@ -1,8 +0,0 @@
-import { CreateEmpresaController } from '../modules/empresa/useCases/createEmpresa/CreateEmpresaController';
-import { Router } from "express";
-
-const empresaRoutes = Router();
-
-empresaRoutes.post("/", new CreateEmpresaController().handle);
-
-export { empresaRoutes };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import { empresaRoutes } from './empresa.routes';
 import { pedidosRoutes } from './pedidos.routes';
 import { pedidoRoutes } from './pedido.routes';
 
 
 const routes = Router();
-
-routes.use("/empresa", empresaRoutes);
 
 routes.use("/pedidos", pedidosRoutes);
 

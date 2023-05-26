d5c08f4456cfaf13ea99c2ec66f56d8cb676880e
Igor Félix <igor.pereira@avenuecode.com>
2023-05-25 21:05:30 -0300

    .

--- src/modules/pedidos/BuscarTodosPedidos/GetAllPedidosController.ts
+++ src/modules/pedidos/BuscarTodosPedidos/GetAllPedidosController.ts
@@ -1,7 +1,7 @@
 import { GetAllPedidosUseCase } from "./GetAllPedidosUseCase";
 import { Request, Response } from "express";
 
-export class GetAllPedidosController {
+export class GetAllPedidosControllerr {
     async handle(req: Request, res: Response) {
 
         const getAllPedidosUseCase = new GetAllPedidosUseCase();
--- src/routes/pedidos.routes.ts
+++ src/routes/pedidos.routes.ts
@@ -1,9 +1,9 @@
 import { GetPedidoPorCpfController } from '../modules/pedidos/buscarPedidosPorCPF/GetPedidoPorCpfController';
-import { GetAllPedidosController } from '../modules/pedidos/buscarTodosPedidos/GetAllPedidosController';
+import { GetAllPedidosControllerr } from '../modules/pedidos/buscarTodosPedidos/GetAllPedidosController';
 import { Router } from "express";
 
 const pedidosRoutes = Router();
 
-pedidosRoutes.get("/all", new GetAllPedidosController().handle);
+pedidosRoutes.get("/all", new GetAllPedidosControllerr().handle);
 pedidosRoutes.get("/:cpf", new GetPedidoPorCpfController().handle);
 export { pedidosRoutes };

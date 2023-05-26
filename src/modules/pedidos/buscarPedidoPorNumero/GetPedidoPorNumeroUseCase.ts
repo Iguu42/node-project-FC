9d3b24337e095c30e74bc8ff2cc70e15b3c9d0d8
Igor Félix <igor.pereira@avenuecode.com>
2023-05-26 00:49:46 -0300

    atualizando retorno de busca

--- src/modules/pedidos/buscarPedidoPorNumero/GetPedidoPorNumeroUseCase.ts
+++ src/modules/pedidos/buscarPedidoPorNumero/GetPedidoPorNumeroUseCase.ts
@@ -10,9 +10,35 @@ export class GetPedidoPorNumeroUseCase {
             include: {
                 cliente: {
                     select: {
-                        nome_completo: true
+                        nome_completo: true,
+                        cpf: true,
+                        telefone: true,
+                        email: true,
+                        endereco: true
+                    }
+                }, produto: {
+                    select: {
+                        nome_produto: true,
+                        referencia: true,
+                        descricao: true,
+                        quantidade: true,
+
+                    }
+                },
+                pagamento: {
+                    select: {
+                        valor: true,
+                        tipo_pagamento: true,
+                        parcela: true,
+                        id_transacao: true
+                    }
+                },
+                pedido_status: {
+                    select: {
+                        status_pedido: true
                     }
                 }
+
             }
         });
 
@@ -20,11 +46,24 @@ export class GetPedidoPorNumeroUseCase {
             return null;
         }
 
+        const moment = require('moment');
         const pedidoFormatado = {
-            id_pedido: pedido.id_pedido,
-            numero_pedido: pedido.numero,
-            data_pedido_realizado: pedido.data_pedido_realizado,
-            cliente_nome_completo: pedido.cliente.nome_completo
+            cpf: pedido.cliente.cpf,
+            nome: pedido.cliente.nome_completo,
+            contato: pedido.cliente.telefone,
+            email: pedido.cliente.email,
+            endereco: pedido.cliente.endereco,
+            numeroDoPedido: pedido.numero,
+            produto: pedido.produto.nome_produto,
+            referencia: pedido.produto.referencia,
+            descricao: pedido.produto.descricao,
+            quantidade: pedido.produto.quantidade,
+            valor_produto: pedido.pagamento.valor / pedido.produto.quantidade,
+            valor_total_produto: pedido.pagamento.valor,
+            tipo_pagamento: pedido.pagamento.tipo_pagamento,
+            parcelas: pedido.pagamento.parcela,
+            id_transacao: pedido.pagamento.id_transacao,
+            dataDaCompra: moment(pedido.data_pedido_realizado).format('DD/MM/YYYY')
         };
 
         return pedidoFormatado;

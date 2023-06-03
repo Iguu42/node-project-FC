20eda8db9b9b5fc918e42bd65174df0180379b3e
Igor Félix <37526093+iguu42@users.noreply.github.com>
2023-06-02 23:17:00 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,13 +10,6 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-
-// Middleware para configurar o stale-while-revalidate
-app.use((req, res, next) => {
-    res.set('Cache-Control', 'public, max-age=60, stale-while-revalidate=300');
-    next();
-});
-
 app.use(routes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -24,12 +17,12 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
         return res.status(err.statusCode).json({
             status: "error",
             message: err.message
-        });
+        })
     }
     return res.status(500).json({
         status: "error",
         message: "Internal server error"
-    });
-});
+    })
+})
 
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () => console.log(`server is running in port ${process.env.PORT}`));

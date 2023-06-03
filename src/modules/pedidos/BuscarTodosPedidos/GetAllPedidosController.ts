7958aa776604c8c12deb08a2a1d9b1e2cacc9a71
Igor Félix <igor.pereira@avenuecode.com>
2023-06-02 22:56:32 -0300

    add cache control

--- src/modules/pedidos/BuscarTodosPedidos/GetAllPedidosController.ts
+++ src/modules/pedidos/BuscarTodosPedidos/GetAllPedidosController.ts
@@ -7,7 +7,7 @@ export class GetAllPedidosController {
         const getAllPedidosUseCase = new GetAllPedidosUseCase();
 
         const result = await getAllPedidosUseCase.execute();
-
+        res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
         return res.status(200).json(result);
     }
 }

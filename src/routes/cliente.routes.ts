21a972282eeaa57fc86c483b992730036a67d6af
Igor Félix <igor.pereira@avenuecode.com>
2023-06-07 12:12:34 -0300

    clear repo

--- src/routes/cliente.routes.ts
+++
@@ -1,8 +0,0 @@
-import { GetPedidoPorCpfController } from '../modules/pedidos/buscarPedidosPorCPF/GetPedidoPorCpfController';
-import { Router } from "express";
-
-const clienteRoutes = Router();
-
-clienteRoutes.get("/:cpf", new GetPedidoPorCpfController().handle);
-
-export { clienteRoutes };
--- src/routes/pedido.routes.ts
+++
@@ -1,7 +0,0 @@
-import { GetPedidoPorNumeroController } from '../modules/pedidos/buscarPedidoPorNumero/GetPedidoPorNumeroController';
-import { Router } from "express";
-
-const pedidoRoutes = Router();
-
-pedidoRoutes.get("/:numbers", new GetPedidoPorNumeroController().handle);
-export { pedidoRoutes };

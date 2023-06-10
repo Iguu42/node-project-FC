b8b869a13ce4ec4ca9517e0761d5dad1bcbbdd96
Igor Félix <igor.pereira@avenuecode.com>
2023-06-10 15:09:00 -0300

    update id type string

--- src/modules/pedidos/comentarios/GetComentariosController.ts
+++ src/modules/pedidos/comentarios/GetComentariosController.ts
@@ -2,12 +2,12 @@ import { Request, Response } from "express";
 import { DeletarComentarioUseCase } from "./DeletarComentarioUseCase";
 import { GetComentariosUseCase } from "./GetComentariosUseCase";
 
-export class GetComentariosController{
+export class GetComentariosController {
     async handle(req: Request, res: Response) {
         const { id } = req.params;
         const getComentariosUseCase = new GetComentariosUseCase();
         try {
-            const comentarios = await getComentariosUseCase.getComentarios(parseInt(id));
+            const comentarios = await getComentariosUseCase.getComentarios(id);
             return res.status(200).json(comentarios);
         } catch (error) {
             console.error("Erro ao buscar comentários:", error);

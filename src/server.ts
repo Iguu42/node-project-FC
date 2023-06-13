c26740b81b20b974fc8bc6273c5b46f00ca3828a
Igor Félix <igor.pereira@avenuecode.com>
2023-06-13 00:59:38 -0300

    add swagger

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,16 @@ import { Request } from 'express';
 import "express-async-errors";
 import express from 'express';
 import { routes } from './routes';
+import swaggerDocs from './swagger.json';
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof AppError) {
